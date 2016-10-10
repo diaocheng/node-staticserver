766ede1b31ddb90f5c5419e1b78de4ceb913b8e8
程刁 <diaocheng@outlook.com>
2016-10-10 23:21:45 +0800

    临时提交

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Server = require('./lib/server.js');
 var port = 8080;
 var app = Server();
-app.set('root', 'lib');
+app.set('root', 'es6tutorial-gh-pages');
 app.listen(port);
 console.log('Server is running at http://127.0.0.1:' + port + '/');
